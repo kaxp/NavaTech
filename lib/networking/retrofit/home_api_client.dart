23d743b80346e829d8f28c5d6f9e4534c80ec3d3
kaxp <kapilsahu232@gmail.com>
2024-11-26 14:34:44 +0530

    Add home api client

---
+++ lib/networking/retrofit/home_api_client.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:tech_nava/modules/home/models/events_response.dart';
+import 'package:tech_nava/networking/constants/network_constants.dart';
+import 'package:tech_nava/networking/models/app_dio.dart';
+
+part 'home_api_client.g.dart';
+
+@RestApi()
+abstract class HomeApiClient {
+  factory HomeApiClient.withDefaultDio({String? baseUrl}) {
+    return HomeApiClient(
+      Modular.get<AppDio>().noAuthDio,
+      baseUrl: NetworkConstants.baseUrl,
+    );
+  }
+
+  factory HomeApiClient(Dio dio, {String? baseUrl}) {
+    return _HomeApiClient(
+      dio,
+      baseUrl: NetworkConstants.baseUrl,
+    );
+  }
+
+  @GET('/albums')
+  Future<List<AlbumsData>> getAlbumsData();
+}
