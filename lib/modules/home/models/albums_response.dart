213ccaded879a643bf11c0db32cb62c1b623c61b
kaxp <kapilsahu232@gmail.com>
2024-11-26 17:56:02 +0530

    Integrate hive type in model class

--- lib/modules/home/models/albums_response.dart
+++ lib/modules/home/models/albums_response.dart
@@ -1,22 +1,28 @@
-// ignore_for_file: must_be_immutable
-
-import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'albums_response.g.dart';
 
+@HiveType(typeId: 0)
 @JsonSerializable()
-class AlbumsResponse extends Equatable {
-  const AlbumsResponse({
+class AlbumsResponse extends HiveObject {
+  AlbumsResponse({
     required this.userId,
     required this.id,
     required this.title,
     this.images = const [], // Default empty list for images
   });
 
+  @HiveField(0)
   final int userId;
+
+  @HiveField(1)
   final int id;
+
+  @HiveField(2)
   final String title;
+
+  @HiveField(3)
   final List<ImageData> images;
 
   factory AlbumsResponse.fromJson(Map<String, dynamic> json) =>
@@ -33,13 +39,13 @@ class AlbumsResponse extends Equatable {
     );
   }
 
-  @override
-  List<Object?> get props => [userId, id, title];
+  List<Object?> get props => [userId, id, title, images];
 }
 
+@HiveType(typeId: 1)
 @JsonSerializable()
-class ImageData extends Equatable {
-  const ImageData({
+class ImageData extends HiveObject {
+  ImageData({
     required this.albumId,
     required this.id,
     required this.title,
@@ -47,10 +53,19 @@ class ImageData extends Equatable {
     required this.thumbnailUrl,
   });
 
+  @HiveField(0)
   final int albumId;
+
+  @HiveField(1)
   final int id;
+
+  @HiveField(2)
   final String title;
+
+  @HiveField(3)
   final String url;
+
+  @HiveField(4)
   final String thumbnailUrl;
 
   factory ImageData.fromJson(Map<String, dynamic> json) =>
@@ -58,6 +73,5 @@ class ImageData extends Equatable {
 
   Map<String, dynamic> toJson() => _$ImageDataToJson(this);
 
-  @override
   List<Object?> get props => [albumId, id, title, url, thumbnailUrl];
 }
--- lib/modules/home/models/albums_response.g.dart
+++ lib/modules/home/models/albums_response.g.dart
@@ -3,6 +3,99 @@
 part of 'albums_response.dart';
 
 // **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class AlbumsResponseAdapter extends TypeAdapter<AlbumsResponse> {
+  @override
+  final int typeId = 0;
+
+  @override
+  AlbumsResponse read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return AlbumsResponse(
+      userId: fields[0] as int,
+      id: fields[1] as int,
+      title: fields[2] as String,
+      images: (fields[3] as List).cast<ImageData>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, AlbumsResponse obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.userId)
+      ..writeByte(1)
+      ..write(obj.id)
+      ..writeByte(2)
+      ..write(obj.title)
+      ..writeByte(3)
+      ..write(obj.images);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AlbumsResponseAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class ImageDataAdapter extends TypeAdapter<ImageData> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ImageData read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ImageData(
+      albumId: fields[0] as int,
+      id: fields[1] as int,
+      title: fields[2] as String,
+      url: fields[3] as String,
+      thumbnailUrl: fields[4] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ImageData obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.albumId)
+      ..writeByte(1)
+      ..write(obj.id)
+      ..writeByte(2)
+      ..write(obj.title)
+      ..writeByte(3)
+      ..write(obj.url)
+      ..writeByte(4)
+      ..write(obj.thumbnailUrl);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ImageDataAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
