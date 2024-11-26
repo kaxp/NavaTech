3bd9c586e089184f205176db043a1c07e3b81649
kaxp <kapilsahu232@gmail.com>
2024-11-26 12:18:12 +0530

    Add constants

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,11 @@
+import 'package:easy_localization/easy_localization.dart';
+
+class AppStrings {
+  static String networkErrorMessage = 'networkErrorMessage'.tr();
+  static String networkRequestCanceled = 'networkRequestCanceled'.tr();
+  static String invalidClientIdError = 'invalidClientIdError'.tr();
+  static String unknownError = 'unknownError'.tr();
+  static String noDataFound = 'noDataFound'.tr();
+  static String back = 'back'.tr();
+  static String retry = 'retry'.tr();
+}
