1e25cd0cb2c4071551848502b85703ce76afe75f
kaxp <kapilsahu232@gmail.com>
2024-11-26 17:56:35 +0530

    Refactor home repo to fetch data from cache or network

--- lib/modules/home/repositories/home_repo.dart
+++ lib/modules/home/repositories/home_repo.dart
@@ -1,14 +1,46 @@
+import 'package:hive/hive.dart';
 import 'package:tech_nava/modules/home/models/albums_response.dart';
 import 'package:tech_nava/networking/retrofit/home_api_client.dart';
 
 class HomeRepo {
   final HomeApiClient _homeApiClient = HomeApiClient.withDefaultDio();
 
+  static const _albumsHiveBoxName = 'albumsBox';
+  static const _imagesHiveBoxName = 'imagesBox';
+
+  /// Fetch albums data either from Hive cache when available or from API
   Future<List<AlbumsResponse>> fetchAlbumsData() async {
-    return _homeApiClient.getAlbumsData();
+    final box = await Hive.openBox<AlbumsResponse>(_albumsHiveBoxName);
+
+    if (box.isNotEmpty) {
+      return box.values.toList();
+    }
+
+    // If no data in Hive, fetch from API
+    final albums = await _homeApiClient.getAlbumsData();
+
+    // Save the fetched data into Hive
+    for (var album in albums) {
+      box.put(album.id, album); // Store album by ID
+    }
+
+    return albums;
   }
 
+  /// Fetch images for a specific album, either from Hive cache when available or from API
   Future<List<ImageData>> fetchAlbumImages({required int albumId}) async {
-    return _homeApiClient.getAlbumImages(albumId: albumId);
+    final imagesBox = await Hive.openBox<List<dynamic>>(_imagesHiveBoxName);
+
+    if (imagesBox.containsKey(albumId)) {
+      return List<ImageData>.from(imagesBox.get(albumId) as List<dynamic>);
+    }
+
+    // If no images in Hive, fetch from API
+    final images = await _homeApiClient.getAlbumImages(albumId: albumId);
+
+    // Store the fetched images in Hive
+    imagesBox.put(albumId, images);
+
+    return images;
   }
 }
